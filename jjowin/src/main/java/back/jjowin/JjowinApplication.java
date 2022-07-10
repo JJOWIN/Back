86b259961f64d299762ba838b9d114007401c28a
eunseo <qnrqjrm03@naver.com>
2022-07-10 15:31:25 +0900

    초기 세팅

---
+++ jjowin/src/main/java/back/jjowin/JjowinApplication.java
@@ -0,0 +1,13 @@
+package back.jjowin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjowinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjowinApplication.class, args);
+	}
+
+}
