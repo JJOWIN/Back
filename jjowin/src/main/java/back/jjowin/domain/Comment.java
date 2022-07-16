fc828f0f93c64fc66929d3e32834a7df217f1b3d
Kim Kang Min <rkdals133@gmail.com>
2022-07-17 01:20:17 +0900

    resolve Feature/#1Entity 생성

---
+++ jjowin/src/main/java/back/jjowin/domain/Comment.java
@@ -0,0 +1,36 @@
+package back.jjowin.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Comment extends BaseTimeEntity{
+    @Id
+    @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "prent_comment_id")
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Comment> childs;
+    //==연관관계 메서드==//
+    public void addChildComment(Comment child){
+        this.childs.add(child);
+        child.setParent(this);
+    }
+}
