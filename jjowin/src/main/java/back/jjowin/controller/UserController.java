3c144bf6e1da11b71091813094cafcead7e8d807
Kim Kang Min <rkdals133@gmail.com>
2022-07-20 21:23:48 +0900

    response body 형식 추가

--- jjowin/src/main/java/back/jjowin/controller/UserController.java
+++ jjowin/src/main/java/back/jjowin/controller/UserController.java
@@ -1,8 +1,11 @@
 package back.jjowin.controller;
 
+import back.jjowin.domain.CustomResponseBody;
 import back.jjowin.domain.User;
 import back.jjowin.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +16,21 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users")
-    public Long signup (@RequestBody User user){
-        return userService.signUp(user);
+    public ResponseEntity signup (@RequestBody User user){
+        CustomResponseBody<User> responseBody = new CustomResponseBody<>("회원가입 성공");
+        try {
+            userService.signUp(user);
+
+        } catch (RuntimeException re){
+            responseBody.setResultCode(-1);
+            responseBody.setResultMsg(re.getMessage());
+            return ResponseEntity.badRequest().body(responseBody);
+        } catch (Exception e){
+            responseBody.setResultCode(-2);
+            responseBody.setResultMsg(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+        }
+
+        return ResponseEntity.ok().body(responseBody);
     }
 }
